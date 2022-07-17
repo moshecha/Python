4e8e8e7940a9ac0712a446f350cc18688f868254
moshecha <109320893+moshecha@users.noreply.github.com>
2022-07-17 19:53:15 -0300

    Add files via upload

---
+++ word_counter.py
@@ -0,0 +1,48 @@
+
+frase = ' '
+simbolos = ['¿', '?', '.', ',', ';', ':', '¡', '!']
+numfrase = 0
+numpalabras = 0
+cant_frases = 0
+total_frases = open('tpFinal/Ej1/phrases.txt')
+for lineas in total_frases:
+    cant_frases += 1
+
+while frase != 'salir' and frase != 'x':
+
+    frase = input('(Para salir ingrese: salir o x) Ingrese una Frase: ')
+    if frase != 'salir' and frase != 'x':
+        texto = open('tpFinal/Ej1/phrases.txt', 'a')
+        # agrega la frase nueva e ingresa una linea vacia
+        texto.write(frase + '\n')
+        texto.close()
+
+        numfrase = frase.split()
+        numfrase = len(numfrase)
+
+        print('---Tu frase contiene %s palabras' % numfrase)
+
+        if cant_frases >= 0:  # si hay frases ingresdas las cuenta
+            cant_frases = 0
+            total_frases = open('tpFinal/Ej1/phrases.txt')
+            for lineas in total_frases:
+                cant_frases += 1
+            print('---En el archivo hay', cant_frases, 'frases')
+
+        with open('tpFinal/Ej1/phrases.txt') as fichero:
+            for linea in fichero:
+                for simbolo in simbolos:
+                    # remplaza los simbolos por espacios
+                    linea = linea.replace(simbolo, ' ')
+                palabras = linea.split()  # hace una lista de palabras
+
+                for palabra in palabras:
+                    numpalabras += 1
+        print('---El archivo contiene %s palabras' % numpalabras)
+        numpalabras = 0
+
+        total_frases = open('tpFinal/Ej1/phrases.txt')
+        for i in total_frases:
+            print('-', i)  # imprime todas las frases
+    else:
+        print('----Finalizado! ----')
